ea971fc24c30b53c94472418d214324c17778000
Piret Valentin <valentin.piret@affinitic.be>
2019-08-02 11:40:13 +0200

    Add robot test

--- src/collective/defaultexcludedfromnav/browser/adapters.py
+++ src/collective/defaultexcludedfromnav/browser/adapters.py
@@ -15,7 +15,8 @@ def default_exclude(context):
 
     request = getRequest()
     portal_type = request.steps[-1].replace('++add++', '')
-    if portal_type in portal_types:
-        return True
+    if portal_types:
+        if portal_type in portal_types:
+            return True
 
     return False
